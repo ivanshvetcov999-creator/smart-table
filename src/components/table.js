58b810bdd5fba738b7d5ccddce6c142c4e358280
Швецов   Иван <ivanshvetcov999@gmail.com>
2026-06-15 19:12:59 +0500

    g0

--- src/components/table.js
+++ src/components/table.js
@@ -1,53 +1,36 @@
 import {cloneTemplate} from "../lib/utils.js";
 
-/**
- * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
- *
- * @param {Object} settings
- * @param {(action: HTMLButtonElement | undefined) => void} onAction
- * @returns {{container: Node, elements: *, render: render}}
- */
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-   before.reverse().forEach(subName => {
+    // @todo: #1.2 — вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {
         root[subName] = cloneTemplate(subName);
         root.container.prepend(root[subName].container); 
     });
 
-    
     after.forEach(subName => {
         root[subName] = cloneTemplate(subName);
         root.container.append(root[subName].container);
     });
 
-
-    // @todo: #1.3 —  обработать события и вызвать onAction()
-    root.container.addEventListener ('change', () => {
-    onAction();
-    });
-
-    root.container.addEventListener ('reset', () => {
-        setTimeout(onAction);
-    });
-
-    root.container.addEventListener ('submit', (e) => {
-    e.preventDefault();
-    onAction(e.submitter);
+    // @todo: #1.3 — обработать события
+    root.container.addEventListener('change', () => { onAction(); });
+    root.container.addEventListener('reset', () => { setTimeout(onAction); });
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
     });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+        // @todo: #1.1 — преобразовать данные в массив строк
         const nextRows = data.map(item => {
             const row = cloneTemplate(rowTemplate);
 
             Object.keys(item).forEach(key => {
                 if (row.elements && row.elements[key]) {
                     const element = row.elements[key];
-                    
-                    
                     if (element.tagName === 'INPUT' || element.tagName === 'SELECT') {
                         element.value = item[key];
                     } else {
@@ -55,9 +38,7 @@ export function initTable(settings, onAction) {
                     }
                 }
             });
-
-            
-            return row.container;
+            return row.container; 
         });
         
         root.elements.rows.replaceChildren(...nextRows);
