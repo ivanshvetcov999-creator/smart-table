453ea8999b8c89bc828e22a72d276c465111ca17
Швецов   Иван <ivanshvetcov999@gmail.com>
2026-07-05 17:41:19 +0500

    proverka

--- src/main.js
+++ src/main.js
@@ -53,8 +53,8 @@ async function render(action) {
   // result = applySorting(result, state, action);
   // result = applyPagination(result, state, action);
   query = applySearching(query, state, action);
-  query = applySearching(query, state, action);
   query = applyFiltering(query, state, action);
+  query = applySorting(query, state, action);
   query = applyPagination(query, state, action);
   const {
     total,
